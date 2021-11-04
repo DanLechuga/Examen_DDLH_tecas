58f34b3fa5f44ab489e5aa018a95c91b392bd3c1
DAN DIDIER LECHUGA HERNANDEZ <DANLECHUGA@live.com>
2021-11-03 22:14:01 -0600

    se agrego validacion, en los retiros y depositos verificando que no se sobregire la cuenta

---
+++ Examen_DDLH_tecas/wwwroot/js/Depositos.js
@@ -0,0 +1,18 @@
+﻿function validarDeposito() {
+    var cantidad = $('#cantidadAManiaobrar').val();
+    if (cantidad < 0) {
+        console.warn("no se puede hacer este movimiento");
+        alert('No se puede depositar' + cantidad+'\n'+'Corriga y vuleva a intentar');
+        $('#BotonS').hide();
+        return false;
+    }
+    console.log("Puede hacer el movimiento");
+    $('#BotonS').show();
+    return true;
+}
+
+function pulsar(e) {
+    if (e.keyCode === 13 && !e.shiftKey) {
+        e.preventDefault();
+    }
+}
---
+++ Examen_DDLH_tecas/wwwroot/js/Retiros.js
@@ -0,0 +1,20 @@
+﻿function VerificaAccion() {
+    var cantidad = $('#cantidadAManiaobrar').val();
+    let saldo = $('#txtSaldo').text();
+    let saldonumero = parseFloat(saldo.substring(1));
+    if (cantidad <= saldonumero) {
+        console.log("Puede retirar");
+        $('#botonS').show();
+        return true;
+    }
+    console.warn("No se puede hacer el movimiento");
+    alert("No se puede retirar : " + cantidad+"\n"+"Verifique la cantidad y vuelva a intentar");
+    $('#botonS').hide();
+    return false;
+}
+function pulsar(e) {
+    if (e.keyCode === 13 && !e.shiftKey) {
+        e.preventDefault();        
+     
+    }
+}
